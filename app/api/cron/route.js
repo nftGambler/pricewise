685fecd1305c296f29cb9f59c734a8aedf824db4
Nft_Gambler <sharo.s.n@hotmail.com>
2024-01-07 19:56:51 -0500

    fix error

--- app/api/cron/route.js
+++ app/api/cron/route.js
@@ -6,7 +6,7 @@ import Product from "@/lib/models/product.model";
 import { NextResponse } from "next/server";
 
 
-export const maxDuration = 300;
+export const maxDuration = 10;
 export const dynamic = 'force-dynamic'
 export const revalidate = 0;
 
